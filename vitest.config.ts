335f0cc90155880a3c2114e1f94dc8ecf6ee4de1
yvygyyth <1589937631@qq.com>
2025-11-17 11:06:01 +0800

    chore: 不强制使用pnpm

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
+        maxWorkers: 1,
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'html']
